602c3a07982303778831cf34f82ad037106ee20c
Maksymilian Debosz <maks.debosz@gmail.com>
2022-02-13 16:04:05 +0100

    Add eslint configuration, tailwindcss

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  "ignorePatterns": [
+    "*.spec.tsx", "*.spec.ts", "**/stories/**",
+  ],
+  "extends": [
+    "react-app",
+    "react-app/jest",
+    "plugin:jsx-a11y/recommended"
+  ],
+  "rules": {
+    "no-shadow": "error",
+    "prefer-const": "warn",
+    "no-console": "warn",
+    "@typescript-eslint/prefer-readonly": "off",
+    "@typescript-eslint/no-implicit-any-catch": "warn",
+    "@typescript-eslint/no-unsafe-call": "warn",
+    "@typescript-eslint/no-unsafe-return": "warn",
+    "@typescript-eslint/no-unnecessary-type-assertion": "warn",
+    "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/no-unused-expressions": "off"
+  },
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "project": "./tsconfig.json",
+    "tsconfigRootDir": __dirname,
+  }
+}
